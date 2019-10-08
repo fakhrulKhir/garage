4fe44d2172ba9bb07eacbc16cc9becb5f3810f4e
Fakhrul Khir <m.fakhrulkhir@gmail.com>
2019-10-08 23:15:18 +0800

    fix directory bash profiles

--- bash/export.sh
+++ bash/export.sh
@@ -2,6 +2,16 @@
 export GOPATH=~/.go
 export PATH=$PATH:$GOPATH/bin
 
+# Export brew settings
+export PATH=/usr/local/sbin:$PATH
+
+# Export PHP 7.2
+export PATH=/usr/local/opt/php@7.2/bin:$PATH
+export PATH=/usr/local/opt/php@7.2/sbin:$PATH
+export PATH=/usr/local/Cellar/php@7.2/7.2.22_1/bin:$PATH
+export LDFLAGS="-L/usr/local/opt/php@7.2/lib"
+export CPPFLAGS="-I/usr/local/opt/php@7.2/include"
+
 # Export Laravel & Composer
 export PATH=~/.config/composer/vendor/bin:$PATH
 export PATH="$PATH:$HOME/.composer/vendor/bin"
